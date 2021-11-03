4b3f3ed07bb5bcb2b88e557374e97f0c5e8b5ac3
leonardBrebene <brebene.leonard@yahoo.ro>
2021-11-03 16:41:20 +0200

    Added Spring Project pincoServer

---
+++ princoServer/src/main/java/com/princo/princoServer/PrincoServerApplication.java
@@ -0,0 +1,13 @@
+package com.princo.princoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincoServerApplication.class, args);
+	}
+
+}
---
+++ princoServer/src/test/java/com/princo/princoServer/PrincoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princo.princoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
