e55e24c125183578efcb5d44ba93aa4e609917cb
leonardBrebene <brebene.leonard@yahoo.ro>
2021-12-18 21:47:55 +0200

    added close button for Accordion

--- princo-client/src/components/FormToAddAPallet.js
+++ princo-client/src/components/FormToAddAPallet.js
@@ -1,4 +1,4 @@
-import { Card, Form, Button, useAccordionButton, Accordion, Stack } from 'react-bootstrap';
+import { Card, Form, Button, useAccordionButton, Accordion, Stack,Nav } from 'react-bootstrap';
 import { useState } from "react";
 import postObject from '../javaScriptComponents/postObject';
 
@@ -24,68 +24,66 @@ const FormToAddAPalet = ({ setTrigerFetch }) => {
 
 
     function CustomToggle({ children, eventKey }) {
-        const decoratedOnClick = useAccordionButton(eventKey, () =>
-            console.log("Custom Toglle"),
-        );
-
+        const decoratedOnClick = useAccordionButton(eventKey);
         return (
-            <Button
-                type="button" variant="outline-dark"
-                onClick={decoratedOnClick}
-            >
-                {children}
-            </Button>
+            <Button type="button" variant="outline-dark" onClick={decoratedOnClick}>{children}</Button>
         );
     }
+    function CloseCustomToggle({ children, eventKey }) {
+        const decoratedOnClick = useAccordionButton(eventKey);
+        return (
+                <Button type="button" variant="outline-danger" size="sm" style={{position:"relative",right:"-40%"}} onClick={decoratedOnClick}>{children}</Button>
+        );
+    }
+
     return (
         <Accordion defaultActiveKey="0" >
             <Card >
                 <Card.Header>
                     <Stack direction="horizontal" gap={3}>
-                        <CustomToggle  eventKey="1" className="bg-light border">Adauga tip palet</CustomToggle>
-                        <CustomToggle  eventKey="2" className="bg-light border">Adauga tip piesa</CustomToggle>
+                        <CustomToggle eventKey="1" className="bg-light border">Adauga tip palet</CustomToggle>
+                        <CustomToggle eventKey="2" className="bg-light border">Adauga tip piesa</CustomToggle>
                     </Stack>
                 </Card.Header>
-                    < Accordion.Collapse eventKey="1">
-                        <Card.Body style={{ maxWidth: '360px', border: '0' }} >
-                            <Form >
-                                <Form.Group className="mb-1" >
-                                    <Form.Label>Creat de</Form.Label>
-                                    <Form.Control size="sm" placeholder="Nume" onChange={(e) => setUserInput(e.target.value)} />
-                                </Form.Group>
+                < Accordion.Collapse eventKey="1">
+                    <Card.Body style={{ maxWidth: '360px', border: '0' }} >
+                        <Form >
+                            <Form.Group className="mb-1" >
+                                <Form.Label>Creat de</Form.Label>
+                                <Form.Control size="sm" placeholder="Nume" onChange={(e) => setUserInput(e.target.value)} />
+                            </Form.Group>
 
-                                <Form.Group className="mb-1" >
-                                    <Form.Label>Nume palet</Form.Label>
-                                    <Form.Control size="sm" placeholder="Nume palet" onChange={(e) => setMaterialInput(e.target.value)} />
-                                </Form.Group>
-                                <Button variant="dark" name="dataOra" onClick={handleSubmitPalet} type="reset">
-                                    Trimite
-                                </Button>
-                            </Form>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                
-                    < Accordion.Collapse eventKey="2">
-                        <Card.Body style={{ maxWidth: '360px', border: '0' }} >
-                            <Form >
-                                <Form.Group className="mb-1" >
-                                    <Form.Label>Responsabil</Form.Label>
-                                    <Form.Control size="sm" placeholder="Responsabil" onChange={(e) => setUserInput(e.target.value)} />
-                                </Form.Group>
+                            <Form.Group className="mb-1" >
+                                <Form.Label>Nume palet</Form.Label>
+                                <Form.Control size="sm" placeholder="Nume palet" onChange={(e) => setMaterialInput(e.target.value)} />
+                            </Form.Group>
+                            <Button variant="dark" name="dataOra" onClick={handleSubmitPalet} type="reset">
+                                Trimite
+                            </Button>
+                            <CloseCustomToggle eventKey="1" >Inchide formular</CloseCustomToggle>
+                        </Form>
+                    </Card.Body>
+                </Accordion.Collapse>
 
-                                <Form.Group className="mb-1" >
-                                    <Form.Label>Nume piesa</Form.Label>
-                                    <Form.Control size="sm" placeholder="Nume piesa" onChange={(e) => setPieceInput(e.target.value)} />
-                                </Form.Group>
-                                <Button variant="dark" name="dataOra" onClick={handleSubmitPiece} type="reset">
-                                    Trimite
-                                </Button>
-                            </Form>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                
-                
+                < Accordion.Collapse eventKey="2">
+                    <Card.Body style={{ maxWidth: '360px', border: '0' }} >
+                        <Form >
+                            <Form.Group className="mb-1" >
+                                <Form.Label>Responsabil</Form.Label>
+                                <Form.Control size="sm" placeholder="Responsabil" onChange={(e) => setUserInput(e.target.value)} />
+                            </Form.Group>
 
+                            <Form.Group className="mb-1" >
+                                <Form.Label>Nume piesa</Form.Label>
+                                <Form.Control size="sm" placeholder="Nume piesa" onChange={(e) => setPieceInput(e.target.value)} />
+                            </Form.Group>
+                            <Button variant="dark" name="dataOra" size="sm" onClick={handleSubmitPiece} type="reset">
+                                Trimite
+                            </Button>
+                            <CloseCustomToggle eventKey="2" >Inchide formular</CloseCustomToggle>
+                        </Form>
+                    </Card.Body>
+                </Accordion.Collapse>
             </Card>
         </Accordion >
     );
